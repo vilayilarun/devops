1092ece2bbb48e8192a3de2697f65559e77718d7
Arun <vilayilarun@gmail.com>
2023-01-31 15:47:00 +0400

    code updated

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -59,7 +59,7 @@ module "eks" {
       max_size     = 3
       desired_size = 2
 
-      instance_types = ["t2.medium"]
+      instance_types = ["t2.large"]
       capacity_type  = "SPOT"
     }
   }
